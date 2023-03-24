4967e341a62c93dfed938b8ecf9efb8d397e927e
gcamaci <77690094+gcamaci@users.noreply.github.com>
2023-03-24 14:40:52 -0500

    formatted artist object with events

--- src/fetchData.js
+++ src/fetchData.js
@@ -5,15 +5,30 @@ const fetchEvents = async (keyword) => {
       const code = data._embedded.attractions[0].id
       const eventResponse = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?attractionId=${code}&apikey=mmsBfHd9j3ILnRlZ21Q1GxO06NY8IVJk`,{mode:'cors'});
       const eventData = await eventResponse.json();
-
+      console.log(data)
       console.log(eventData)
-    
-      return data;
+      console.log(formatEvents(eventData._embedded.events))
+      return {
+        name: data._embedded.attractions[0].name,
+        id_code: data._embedded.attractions[0].id,
+        upcoming_events: formatEvents(eventData._embedded.events)
+      };
     } catch (error) {
       console.error('Error fetching events:', error);
       throw error;
     }
 };
 
+
+function formatEvents(shows){
+  const formattedShows = shows.map((show) => {
+    return {
+      date: show.dates.start.localDate,
+      time: show.dates.start.localTime,
+      venue: show._embedded.venues[0].name
+    }
+  });
+  return formattedShows
+}
 export {fetchEvents}
 
